8f64a1fca7f92d0c6acf3bdd63fc9a2459007600
hacker <hacker20190387468675@gmail.com>
2019-10-06 13:41:54 +0200

    update

--- welcome.php
+++ welcome.php
@@ -35,7 +35,6 @@ if(isset($_POST['submit_text'])){
       }
       shell_exec('sudo chmod 777 .');
       $file = './name.txt';
-
       // Ouvre un fichier pour lire un contenu existant
       $current = file_get_contents($file, FILE_USE_INCLUDE_PATH | FILE_APPEND);
       chmod($current, 0777);
@@ -51,7 +50,6 @@ if(isset($_POST['submit_text'])){
       //chmod($current, 0777);
       //print_r('file empty?');
       //print_r($current);
-
       $file = './new_text.txt';
       shell_exec('sudo chmod 777 .');
       $current = file_get_contents($file, FILE_USE_INCLUDE_PATH);
@@ -84,11 +82,9 @@ if(isset($_POST['submit_date'])){
       print_r('<h1>msg sent!</h1>');
       foreach($errors as $errorMessage){
         print_r($errorMessage . '<br>');
-
       }
       shell_exec('sudo chmod 777 .');
       $file = './date_for_data_entry.txt';
-
       // Ouvre un fichier pour lire un contenu existant
       $current = file_get_contents($file);
       chmod($current, 0777);
@@ -105,7 +101,6 @@ if(isset($_POST['add_new_words'])){
       shell_exec('rm items.json');
       shell_exec('rm database.json');
       $file = './new_text.txt';
-
       // Ouvre un fichier pour lire un contenu existant
       $current = file_get_contents($file);
       chmod($current, 0777);
@@ -115,7 +110,6 @@ if(isset($_POST['add_new_words'])){
       $parts = preg_split('/,.;:|\s/', $current, null, PREG_SPLIT_NO_EMPTY);
       //remove duplicate
       $wordsWoDuplicate = array_unique($parts);
-
       foreach ($words_wo_duplicate as &$value) {
         $value=preg_replace('/[^a-zA-Z0-9_ %\[\]\(\)%&-]/s', '', $value);
         $value=strtolower($value);
@@ -216,16 +210,13 @@ if(isset($_POST['add_new_words'])){
       $current = file_get_contents($json);
       chmod($current, 0777);
       $current .= json_encode($jsonData, JSON_PRETTY_PRINT); 
-
       file_put_contents($json, $current);
       //==================================================================// 
-
       //same process, but exporting the file to be used by the app instead 
       shell_exec('sudo chmod 777 .');
       $json = file_get_contents('./items.json');
       chmod($json, 0777);
       $jsonData = json_decode($json);
-
       //if ($jsonData === null) {
         //$items = array();
         //$forMerge[] = $items;
@@ -242,7 +233,6 @@ if(isset($_POST['add_new_words'])){
           $jsonData['items'][$i] = $dataArray;
           print_r("here");
       }
-
       //===============WRITES JSON FILE FOR THE APP=============//
       //print_r($jsonData);
       shell_exec('sudo chmod 777 .');
@@ -254,21 +244,17 @@ if(isset($_POST['add_new_words'])){
       $current = file_get_contents($file);
       chmod($current, 0777);
       $current .= json_encode($jsonData, JSON_PRETTY_PRINT);
-
       file_put_contents($file, $current);
       //===============END WRITE JSON FILE FOR THE APP==========/
       shell_exec('sudo cp -r items.json public/src.json');
       shell_exec('npm run start');
-
 }
       
 if(isset($_POST['database_with_date'])){
-
       //===OPEN AND READS THE FILE CONTAINING THE DATA ENTRY DATE====//
       shell_exec('sudo chmod 777 .');
       shell_exec('sudo rm items.json');
       $file = './date_for_data_entry.txt';
-
       $dateForDataEntry = file_get_contents($file);
       chmod($dateForDataEntry, 0777);
       //===END OPEN AND READS THE FILE CONTAINING THE DATA ENTRY DATE====//
@@ -280,7 +266,6 @@ if(isset($_POST['database_with_date'])){
       chmod($json, 0777);
       
       $jsonData = json_decode($json,true);
-
       shell_exec('sudo chmod 777 .');
       $json = "./items.json";
       $jsonContent = file_get_contents($json);
@@ -288,7 +273,6 @@ if(isset($_POST['database_with_date'])){
       $jsonDataEntry = json_decode($jsonContent, true);
       //==========END OPENS AND READS THE BIG DATABASE FILE====//
       //======ADDS ITEMS ARRAY TO JSON DATA ARRAY=====//
-
       //if ($jsonData === null) {
         //$jsonData = array();
         //$items = array();
@@ -333,128 +317,4 @@ if(isset($_POST['database_with_date'])){
       file_put_contents($json, $current);
       //=============END WRITES THE WORDS WITH THE RIGHT DATA ENTRY DATES INTO THE FILE==============//
       shell_exec('npm run start');
-
 }
-/*
-//displays list of words
-//print_r($words_wo_duplicate); 
-//creation of the  "words" column
-//end of creation of "mp3" column
-//print_r($mp3);
-//create array containing mp3 file names
-//installation of a time zone  
-//date_default_timezone_set('UTC');
-// date of the day 
-//$date_of_today_s_practice = array();
-//$today = date("Ymd");
-//print the date of today
-//print_r($today);
-//$ids = array(1,2,3);
-//$result = array();
-//foreach ($words_wo_duplicate as $id => $key) {
-//    $result[$key] = array(
-//        'word'  => $words_wo_duplicate[$id],
-//        'mp3 filename' => "$words_wo_duplicate[$id].mp3",
-        //'date of today'    => $today,
-//        'date of today'    => '20190101', 
-//    );
-    //$csv.="$words_wo_duplicate[$id],$words_wo_duplicate[$id].mp3,$today\n";
-//    $csv.="$words_wo_duplicate[$id],$words_wo_duplicate[$id].mp3,20190501\n";
-//}
-//$ar = range(0,count($words));
-//print_r($ar);
-//$row = array();
-//$mychart = array();
-//foreach ($ar as &$value) {
- //   $val=$value;
-    //$row=array();
-  //  print_r($val);
-    //array_push($row,$words[$ar]);
-    //array_push($row,$mp3files[$ar]);
-    //array_push($row,$dates[$ar]);
-    //print_r($row);
-    //array_push($mychart,$row);
-   // $row=array();
-//}
-//print_r($result);
-//print_r($csv);
-//file_put_contents("2.txt", $csv);
-/////// dedoublonner deux BDDS en fusionnant leurs collonnes des dates
-//$input1 = file_get_contents( '1.txt' ); // get the contents,
-//$input2 = file_get_contents( '2.txt' ); // get the contents,
-//separation of the characters string  gives list of words 
-//$file1_line_by_line = preg_split('/\s/', $input1, null, PREG_SPLIT_NO_EMPTY);
-//$file1_line_by_line = preg_split("/[\n]+/", $input1);
-//$file2_line_by_line = preg_split("/[\n]+/", $input2);
-//$file2_line_by_line = preg_split('/\s/', $input2, null, PREG_SPLIT_NO_EMPTY);
-//liste de mots du premier texte
-//$list_of_words_1=array();
-//liste de mots du deuxieme texte
-//$list_of_words_2=array();
-foreach ($file1_line_by_line as &$value) {
-  $word1=array(); 
-  $word1 = preg_split('/[,]+/', $value, null, PREG_SPLIT_NO_EMPTY);
-  reset($word1);
-  //print_r(current($word1));
-  array_push($list_of_words_1, current($word1));
-}
-print_r("file 2 line");
-print_r($file2_line_by_line[8]);
-foreach ($file2_line_by_line as &$value) {
-  $word2=array();   
-  $word2 = preg_split('/[,]+/', $value, null, PREG_SPLIT_NO_EMPTY);
-  //print_r($word2);
-  reset($word2);
-  //print_r(current($word2)); 
-  array_push($list_of_words_2, current($word2));
-}
-date_default_timezone_set('UTC');
-// 10, 3, 2001
-$today = date("Ymd");          
-//$list_of_words_1=array_filter($list_of_words_1());
-//$list_of_words_2=array_filter($list_of_words_2());
-$csvlines=array();
-$inter = array_uintersect($list_of_words_1, $list_of_words_2, "strcasecmp");
-$diff = array_values($inter);
-//$diff=array_filter($diff);
-foreach ($diff as &$word) {
-  $key1 = array_search($word, $list_of_words_1);
-  $key2 = array_search($word, $list_of_words_2);
-  $line_to_add =$file1_line_by_line[$key1];
-  $line_to_add=str_replace("\r\n","",$line_to_add);
-  $emptystr=$line_to_add;
-  $line_to_add.=",$today";
-  array_push($csvlines, $line_to_add);
-  if ($emptystr=="") {
-    $test=array_pop($csvlines); 
-  }
-//add a date at the end of the line for the duplicate (remains the the duplicate word)
-  unset($list_of_words_1[$key1]);
-  unset($list_of_words_2[$key2]);
-}
-foreach ($list_of_words_1 as &$word) {
-  $key = array_search($word, $list_of_words_1);
-  $line_to_add =$file1_line_by_line[$key];
- 
-  array_push($csvlines, $line_to_add);
-  unset($list_of_words_1[$key]);
-  //unset($list_of_words_2[$key]);
-}
-foreach ($list_of_words_2 as &$word) {
-  
-  $key = array_search($word, $list_of_words_2);
-  $line_to_add =$file2_line_by_line[$key];
-  array_push($csvlines, $line_to_add);
-  //unset($list_of_words_2[$key]);
-}
-//$csvlines = array_filter($csvlines);
-//$csvlines = y.map(el => el.trim());
-$csvlines = array_filter(array_map('trim',$csvlines));
-$csvlines = array_unique($csvlines);
-print_r($csvlines);
-$csv="";
-foreach ($csvlines as &$value) {
-  $csv.="$value\n"; 
-}
-file_put_contents("final.txt", $csv);*/
-?>
